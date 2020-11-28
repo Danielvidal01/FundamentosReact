c6c4fb34fc7fb406d6433baef6d36afedc67bb3e
Daniel Vidal <danielvidalDev@outlook.com>
2020-11-28 10:13:31 -0300

    Updates

---
+++ precache-manifest.73f78f8e278853e03b9913c5ce348d23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ecf55cdb953e3423effb086939e6058",
+    "url": "/FundamentosReact/index.html"
+  },
+  {
+    "revision": "e530ed83eee0fe693890",
+    "url": "/FundamentosReact/static/css/main.eef3f9fe.chunk.css"
+  },
+  {
+    "revision": "cab8c7d2845bb104b3c3",
+    "url": "/FundamentosReact/static/js/2.f98150ca.chunk.js"
+  },
+  {
+    "revision": "2f0a7e9e9fc7289c4c641befcd7fe287",
+    "url": "/FundamentosReact/static/js/2.f98150ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e530ed83eee0fe693890",
+    "url": "/FundamentosReact/static/js/main.d505ea1c.chunk.js"
+  },
+  {
+    "revision": "62b64b3c19137e0e739c",
+    "url": "/FundamentosReact/static/js/runtime-main.26bd488a.js"
+  }
+]);
